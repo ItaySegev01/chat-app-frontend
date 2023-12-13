6a16151e129f5bb748a14877a149bd3bc70f57f0
Itay Segev <itaysegev123@gmail.com>
2023-12-13 17:27:38 +0200

    .

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,6 +1,6 @@
-import { io } from "socket.io-client";
-import React from "react";
-export const API_URL = "http://localhost:5000";
+import { io } from 'socket.io-client';
+import React from 'react';
+export const API_URL = 'https://chat-app-api-1.up.railway.app';
 export const socket = io(API_URL);
 // app context
 export const AppContext = React.createContext();
