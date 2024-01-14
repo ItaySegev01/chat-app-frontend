d6622ea44a07d03650544434b1dd258260a3711b
Itay Segev <itaysegev123@gmail.com>
2024-01-14 16:37:35 +0200

    [Logic] - added a .env file

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import React from 'react';
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 export const socket = io(API_URL);
 // app context
 export const AppContext = React.createContext();
